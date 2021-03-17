f7aec9d1a780d90e1dc6948147c88f988467ed15
{CodeZone} <50874934+appu22@users.noreply.github.com>
2021-03-17 20:18:33 +0530

    java script
    java script 
    print statements
    variable declaration
    how many ways to write java script 
    /*Read commits also*/

---
+++ 17-03-2021/first.js
@@ -0,0 +1,65 @@
+
+console.log("welcome........");
+document.write("<h1>Welcome to java Script</h1>")
+
+// alert("hiiii i am appu")
+
+/* how to decalare variable
+ var variablename= value;
+
+ Data Types in Java Script 
+ Number
+ String 
+ boolean
+ undefined
+ null
+*/
+
+
+var b;
+var a=10;
+console.log(typeof(a))
+console.log(a);
+console.log(b);
+
+a="java script"
+console.log(typeof(a))
+console.log(" A value is "+a)
+
+a=true
+console.log(typeof(a))
+console.log(a)
+
+var a;
+console.log(typeof(a))
+console.log(a)
+
+a=10
+if(a==10){
+        console.log("equal")
+}else{
+    console.log(" not equal")
+}
+// for loop -------------------------------------
+for(var i=0; i<=10; i++){
+    console.log(i)
+}
+// alert(console.log);
+// funtion  u can call fucntuon name 
+function first(){
+    console.log(" inside function")
+}
+first() 
+/* disp()
+console.log(disp)--it will be print entier function
+*/
+//it is also give the output before decalare function u call 
+second()
+function second(){
+    console.log(" inside function")
+}
+ console.log(s)
+
+
+
+
